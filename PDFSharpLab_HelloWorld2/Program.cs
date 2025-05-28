a21acef37a9d0b23b5d774c16114b534d82227cc
Rely Ky <relyky@hotmail.com>
2025-05-28 16:34:44 +0800

    試著簽章

--- PDFSharpLab_HelloWorld2/Program.cs
+++ PDFSharpLab_HelloWorld2/Program.cs
@@ -1,7 +1,26 @@
 ﻿using PdfSharp.Drawing;
 using PdfSharp.Fonts;
 using PdfSharp.Pdf;
+using PdfSharp.Pdf.Signatures;
 using PDFSharpLab_HelloWorld2;
+using System.Security.Cryptography.X509Certificates;
+
+static X509Certificate2 GetCertificate()
+{
+  DirectoryInfo certFolder = new DirectoryInfo(@"assets/pdfsharp-6.x/signatures"); // 取得字型資源路徑
+
+  var pfxFile = Path.Combine(certFolder.FullName , "test-cert_rsa_1024.pfx");
+  var rawData = File.ReadAllBytes(pfxFile);
+
+  // This code is for demonstration only. Do not use password literals for real certificates in source code.
+  var certificatePassword = "Seecrit1243";
+
+  var certificate = new X509Certificate2(rawData,
+      certificatePassword,
+      X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+
+  return certificate;
+}
 
 // 註冊自訂字型解析器
 GlobalFontSettings.FontResolver = new CustomFontResolver();
@@ -11,6 +30,7 @@ GlobalFontSettings.UseWindowsFontsUnderWindows = true;
 // 建立 PDF 文件
 using PdfDocument document = new PdfDocument();
 document.Info.Title = "PDFsharp 中文練習";
+document.Info.Subject = "PDFsharp 中文練習主旨";
 
 PdfPage page = document.AddPage();
 XGraphics gfx = XGraphics.FromPdfPage(page);
@@ -32,7 +52,22 @@ gfx.DrawString($"這是標楷體 36 PDFsharp at {DateTime.Now:HH:mm:ss} ！", fo
 XFont fontArial = new XFont("Arial", 36);
 gfx.DrawString($"This is Arial font, 36 PDFsharp at {DateTime.Now:HH:mm:ss} ！", fontArial, XBrushes.Black, 10, 140);
 
+//## 試著簽章
+var signPosition = gfx.Transformer.WorldToDefaultPage(new XPoint(144, 600));
+var pdfSignatureHandler = DigitalSignatureHandler.ForDocument(document,
+    new PdfSharpDefaultSigner(GetCertificate(), PdfMessageDigestType.SHA256),
+    new DigitalSignatureOptions
+    {
+      ContactInfo = "Sky-Walker Kao",
+      Location = "新北市",
+      Reason = "測試 PDF 簽章",
+      Rectangle = new XRect(signPosition.X, signPosition.Y, 200, 50), // 簽章位置
+      AppearanceHandler = new SignatureAppearanceHandler("高天賜\n新北市")
+    });
+
 // 儲存 PDF
 string filename = "output.pdf";
 document.Save(filename);
 Console.WriteLine($"PDF 已儲存為 {filename}");
+
+
---
+++ PDFSharpLab_HelloWorld2/SignatureAppearanceHandler.cs
@@ -0,0 +1,47 @@
+﻿using PdfSharp.Drawing.Layout;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf.Annotations;
+using PdfSharp.Quality;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFSharpLab_HelloWorld2;
+
+class SignatureAppearanceHandler : IAnnotationAppearanceHandler
+{
+  private string _signatureText = "簽名";
+
+  public SignatureAppearanceHandler(string signatureText)
+  {
+    _signatureText = signatureText;
+  }
+
+  public void DrawAppearance(XGraphics gfx, XRect rect)
+  {
+    DirectoryInfo imageFolder = new DirectoryInfo(@"assets/pdfsharp-6.x/signatures"); // 取得字型資源路徑
+
+    var image = XImage.FromFile(Path.Combine(imageFolder.FullName, "JohnDoe.png"));
+
+    //string text = "John Doe\nSeattle, " + DateTime.Now.ToString(CultureInfo.GetCultureInfo("EN-US"));
+    string text = $"{_signatureText}, {DateTime.Now:F}";
+
+    //var font = new XFont("Verdana", 7.0, XFontStyleEx.Regular);
+    var font = new XFont("標楷體", 8.0, XFontStyleEx.Regular);
+    var textFormatter = new XTextFormatter(gfx);
+    double num = (double)image.PixelWidth / image.PixelHeight;
+    double signatureHeight = rect.Height * .4;
+    var point = new XPoint(rect.Width / 10, rect.Height / 10);
+
+    // Draw image.
+    gfx.DrawImage(image, point.X, point.Y, signatureHeight * num, signatureHeight);
+    // Adjust position for text. We draw it below image.
+    point = new XPoint(point.X, rect.Height / 2d);
+
+    //textFormatter.DrawString(text, font, new XSolidBrush(XColor.FromKnownColor(XKnownColor.Black)), new XRect(point.X, point.Y, rect.Width, rect.Height - point.Y), XStringFormats.TopLeft);
+    textFormatter.DrawString(text, font, XBrushes.Black, new XRect(point.X, point.Y, rect.Width, rect.Height - point.Y), XStringFormats.TopLeft);
+  }
+}
